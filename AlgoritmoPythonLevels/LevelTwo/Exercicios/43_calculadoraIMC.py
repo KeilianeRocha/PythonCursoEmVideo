f040dffe1b266ecfa189ab6c13785cb827ebdc73
KeilianeRocha <keilianesrocha@gmail.com>
2025-03-10 16:21:15 -0300

    exe 43 e 44 v1.1

--- AlgoritmoPythonLevels/LevelTwo/Exercicios/43_calculadoraIMC.py
+++ AlgoritmoPythonLevels/LevelTwo/Exercicios/43_calculadoraIMC.py
@@ -1,11 +1,11 @@
 # TODO: Desenvolva uma lógica que leia o peso e a altura de uma pessoa,
-# calcule seu IMC e mostre seu status, de acordo com a tabela abaixo;
-# - Abaixo de 18.5 ==> Abaixo do peso
-# - entre 18.9 e 24.9 ==> Peso ideal
-# - entre 25.9 e 29.9 ==> Sobrepeso
-# - entre 30. e 34.9 ==> Obesidade classe I
-# - entre 35.0 e 39.9 ==> Obesidade classe II
-# - Acima de 40 ==> Obesidade classe III
+    # calcule seu IMC e mostre seu status, de acordo com a tabela abaixo;
+    # - Abaixo de 18.5 ==> Abaixo do peso
+    # - entre 18.9 e 24.9 ==> Peso ideal
+    # - entre 25.9 e 29.9 ==> Sobrepeso
+    # - entre 30. e 34.9 ==> Obesidade classe I
+    # - entre 35.0 e 39.9 ==> Obesidade classe II
+    # - Acima de 40 ==> Obesidade classe III
 
 while True:
     peso = float(input("Digite seu peso (Kg) "))
